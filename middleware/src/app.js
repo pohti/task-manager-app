4a4d672e08d68e1c6a59af5083db46b15c48f4c0
Poh <minmarnoo@gmail.com>
2020-10-03 22:16:52 +0800

    headers

--- middleware/src/app.js
+++ middleware/src/app.js
@@ -20,7 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 app.use(cors(settings));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 // create router
 // setup what the route will do
